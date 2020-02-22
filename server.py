ab358c337f92d32a52003a4af92c87bd2ef2fd39
Kristy <qtbeeestmoi@gmail.com>
2020-02-21 21:45:12 -0800

    Find out multiprocessing is a better thing to use for this thing

--- server.py
+++ server.py
@@ -1,62 +1,84 @@
 import board
 import neopixel
 import atexit
+import time
+from multiprocessing import Pipe, Process
+from multiprocessing.connection import Connection
 
-from light_thread import LightThread
 from flask import Flask
 from critmas import Critmas
 from twinkle_white import TwinkleWhite
 from white_scroll import WhiteScroll
 from pew_with_fade import PewWithFade
+from rainbow_across import RainbowAcross
+from rainbow_in_place import RainbowInPlace
+from rainbow_random import RainbowRandom
+from utils import set_pixels, LightController
 
 # flask setup
 app = Flask(__name__)
 
 # light setup
 num_pixels = 50
-pixels = neopixel.NeoPixel(
-    board.D18,
-    num_pixels,
-    brightness=0.1,
-    auto_write=False,
-    pixel_order=neopixel.RGB)
-lightThread = LightThread(TwinkleWhite(num_pixels), pixels)
-lightThread.run()
+global pixels
 
 
-# to make sure the lights get shut off on normal exit
-@atexit.register
+# Function that will be running under its own process
+def lightLoop(recv: Connection):
+    print("starting that process...")
+    lightPattern = LightController(num_pixels)
+    while True:
+        if recv.poll():
+            print("get new pattern")
+            lightPattern = recv.recv()
+
+        pixel_values = lightPattern.pixels_for_frame()
+        set_pixels(pixels, pixel_values)
+        lightPattern.update_frame()
+        time.sleep(lightPattern.time_to_sleep)
+
+
+# for freeing the pin and turning off the lights
 def onExit():
+    p.terminate()
     pixels.deinit()
 
 
 @app.route('/pattern/<pattern>', methods=['POST'])
 def usePattern(pattern=None):
+    global input_connection
     if pattern == "critmas":
-        lightThread.setPattern(Critmas(num_pixels))
+        input_connection.send(Critmas(num_pixels))
     elif pattern == "twinkle":
-        lightThread.setPattern(TwinkleWhite(num_pixels))
+        input_connection.send(TwinkleWhite(num_pixels))
     elif pattern == "white_scroll":
-        lightThread.setPattern(WhiteScroll(num_pixels))
+        input_connection.send(WhiteScroll(num_pixels))
     elif pattern == "pew_with_fade":
-        lightThread.setPattern(PewWithFade(num_pixels))
+        input_connection.send(PewWithFade(num_pixels))
+    elif pattern == "rainbow_across":
+        input_connection.send(RainbowAcross(num_pixels))
+    elif pattern == "rainbow_in_place":
+        input_connection.send(RainbowInPlace(num_pixels))
+    elif pattern == "rainbow_random":
+        input_connection.send(RainbowRandom(num_pixels))
     else:
         return "pattern is not known"
     return pattern
 
 
-# def main():
-#     try:
-#         while True:
-#             pixel_values = lightPattern.pixels_for_frame()
-#             set_pixels(pixels, pixel_values)
-#             lightPattern.update_frame()
-#             time.sleep(lightPattern.time_to_sleep)
+if __name__ != '__main__':
+    print("__name__ is ", __name__)
+    global pixels, input_connection
+    pixels = neopixel.NeoPixel(
+        board.D18,
+        num_pixels,
+        brightness=0.1,
+        auto_write=False,
+        pixel_order=neopixel.RGB)
 
-#     except (KeyboardInterrupt, SystemExit):
-#         pixels.deinit()
+    atexit.register(onExit)
 
+    child_conn, input_connection = Pipe(False)
 
-if __name__ == '__main__':
-    app.run()
-#     main()
+    p = Process(target=lightLoop, args=(child_conn,))
+    p.start()
