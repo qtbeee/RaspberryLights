6edfbf4a802a098a8acc49d96da2399a231926f1
Kristy <qtbeeestmoi@gmail.com>
2020-02-22 18:37:58 -0800

    Move python stuff into its own folder

--- server/server.py
+++ server/server.py
@@ -4,8 +4,8 @@ import atexit
 import time
 from multiprocessing import Pipe, Process
 from multiprocessing.connection import Connection
+from flask import Flask, jsonify, abort, request
 
-from flask import Flask
 from critmas import Critmas
 from twinkle_white import TwinkleWhite
 from white_scroll import WhiteScroll
@@ -21,6 +21,17 @@ app = Flask(__name__)
 # light setup
 num_pixels = 50
 global pixels
+
+# available light patterns
+light_patterns = {
+    "critmas": {"class": Critmas, "canChooseColor": False},
+    "twinkle": {"class": TwinkleWhite, "canChooseColor": False},
+    "whiteScroll": {"class": WhiteScroll, "canChooseColor": False},
+    "pewWithFade": {"class": PewWithFade, "canChooseColor": False},
+    "rainbowAcross": {"class": RainbowAcross, "canChooseColor": False},
+    "rainbowInPlace": {"class": RainbowInPlace, "canChooseColor": False},
+    "rainbowRandom": {"class": RainbowRandom, "canChooseColor": False},
+}
 
 
 # Function that will be running under its own process
@@ -44,26 +55,33 @@ def onExit():
     pixels.deinit()
 
 
-@app.route('/pattern/<pattern>', methods=['POST'])
-def usePattern(pattern=None):
-    global input_connection
-    if pattern == "critmas":
-        input_connection.send(Critmas(num_pixels))
-    elif pattern == "twinkle":
-        input_connection.send(TwinkleWhite(num_pixels))
-    elif pattern == "white_scroll":
-        input_connection.send(WhiteScroll(num_pixels))
-    elif pattern == "pew_with_fade":
-        input_connection.send(PewWithFade(num_pixels))
-    elif pattern == "rainbow_across":
-        input_connection.send(RainbowAcross(num_pixels))
-    elif pattern == "rainbow_in_place":
-        input_connection.send(RainbowInPlace(num_pixels))
-    elif pattern == "rainbow_random":
-        input_connection.send(RainbowRandom(num_pixels))
+@app.route('/pattern', methods=['GET'])
+def getPatterns():
+    return {
+        "patterns": [
+            {
+                "pattern": key,
+                "canChooseColor": value.get("canChooseColor")
+            } for key, value in light_patterns.items()
+        ]
+    }
+    # return jsonify({'patterns': [*light_patterns]})
+
+
+@app.route('/pattern', methods=['POST'])
+def usePattern():
+    if request.is_json:
+        body = request.get_json()
+        patternName = body.get("pattern")
+
+        pattern = light_patterns[patternName]
+        if pattern is not None:
+            input_connection.send(pattern.get("class")(num_pixels))
+        else:
+            return abort(422)
+        return patternName
     else:
-        return "pattern is not known"
-    return pattern
+        abort(400)
 
 
 if __name__ != '__main__':
